ea67ff449dfbc4083a18f63448edab91d9c70c8f
Nafly09 <juniorinterlyte95@hotmail.com>
2021-11-07 21:07:46 -0300

    button test finalized

---
+++ src/__tests__/button.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Button from "../components/Search";
+import Input from "../components/Search";
+
+describe("Button Component", () => {
+  it("should be able to render an Button", () => {
+    render(<Button onClick={() => {}} />);
+
+    expect(screen.getByRole("button")).toBeTruthy();
+  });
+});
